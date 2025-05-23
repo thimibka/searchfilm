bfa204c1d16bdbbf2ffee1b14ad900f35c010bbf
Mika <fseetp@gmail.com>
2025-05-23 10:04:37 +0200

    launch

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Nom de ton app",
+        short_name: "TonApp",
+        description: "Description de ton app",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "icons/camera.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/camera.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
